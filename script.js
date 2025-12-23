59a981cbc9f2988f45f5a8492bc667b8a316ef75
Meche221 <binaymeche22@gmail.com>
2025-12-23 20:03:18 +0545

    handling the outGoing messages from user

---
+++ script.js
@@ -0,0 +1,39 @@
+const chatBody = document.querySelector(".chat-body");
+const messageInput = document.querySelector(".message-input");
+const sendMessageButton = document.querySelector("#send-message");
+
+const userData = {
+  message: null
+}
+
+// Create message element with dynamic classes and return it
+const createMessageElement = (content, classes) => {
+  const div = document.createElement("div");
+  div.classList.add("message", classes);
+  div.innerHTML = content;
+  return div;
+}
+
+//Handle outgoing user messages
+const handleOutgoingMessage = (e) => {
+  e.preventDefault();
+  userData.message = messageInput.value.trim();
+  messageInput.value = "";
+
+  //Create and display user message
+  const messageContent = `<div class="message-text"></div>`;
+
+  const outgoingMessaegDiv = createMessageElement(messageContent, "user-message");
+  outgoingMessaegDiv.querySelector(".message-text").innerText = userData.message;
+  chatBody.appendChild(outgoingMessaegDiv);
+}
+
+//Handle Enter key press for sending message
+messageInput.addEventListener("keydown", (e) => {
+  const userMessage = e.target.value.trim() ;
+  if(e.key === "Enter" && userMessage){
+    handleOutgoingMessage(e);
+  }
+});
+
+sendMessageButton.addEventListener("click", (e)=> handleOutgoingMessage(e))
